822a6749b790a80d300e73ddbeeab3b55ff15377
nsaragosaharris <nsaragosaharris@ucla.edu>
2022-07-29 14:16:57 -0700

    Fixing duplicates.

--- code/define_dataframes.R
+++ code/define_dataframes.R
@@ -146,17 +146,23 @@ data$pds_p_ss_category <- relevel(data$pds_p_ss_category, ref="Pre")
 #data$race.ethnicity.5level[(data$race.eth.7level == "AIAN" | data$race.eth.7level == "NHPI")] = "Other"
 #data$race.ethnicity.5level = droplevels(data$race.ethnicity.5level)
 
-# Exclude two participants who have multiple rows for baseline.
-#data <- subset(data, src_subject_id != "NDAR_INV2ZA2LC3N" & src_subject_id != "NDAR_INVJ9GNXGK5")
-# Also need to exclude NDAR_INV3E0WVH3G; but will remove duplicate rows instead of removing them manually (and will keep one row for each of them rather than remove all their rows).
+# Because of duplicate rows, these people were unintentionally added to both samples.
+# Make sure they are only in one sample here.
+if(phase_folder == "exploratory" ){
+  data <- subset(data, src_subject_id != "NDAR_INV2ZA2LC3N")
+}else if (phase_folder == "confirmatory"){
+  data <- subset(data, src_subject_id != "NDAR_INVJ9GNXGK5")
+}
+
+# Also need to exclude duplicated rows for a couple people, but will remove duplicate rows instead of removing them manually (and will keep one row for each of them rather than remove all their rows).
 
 # Use only data from baseline.
 data <- subset(data,eventname == "baseline_year_1_arm_1")
-nrow(data) # 5961 (exploratory).
+nrow(data) # 5953 (exploratory); 5960 (confirmatory).
 
 # Get rid of duplicated rows.
 data <- data[!duplicated(data$src_subject_id),]
-nrow(data) # 5940 (exploratory).
+nrow(data) # 5939 (exploratory and confirmatory).
 
 data$PDS_score_z<- scale(data$PDS_score)
 data$cbcl_scr_syn_internal_r_z <- scale(data$cbcl_scr_syn_internal_r)
